c9400fc516fc46a6cb380d6fb719242d2f55e325
Kevin <kevinsebcam@gmail.com>
2021-02-01 23:21:52 -0500

    Create website portfolio

---
+++ svgPortraitReactions.js
@@ -0,0 +1,23 @@
+function addPortraitReactions() {
+  let contactMeButton = document.getElementById("contact-me");
+  let resumeButton = document.getElementById("resume");
+
+  let starEyes = document.getElementById("Star_Eyes");
+  let shades = document.getElementById("Shades");
+
+  contactMeButton.addEventListener("mouseenter", function (event) {
+    starEyes.style.display = "block";
+  })
+
+  contactMeButton.addEventListener("mouseleave", function (event) {
+    starEyes.style.display = "none";
+  })
+
+  resumeButton.addEventListener("mouseenter", function (event) {
+    shades.style.display = "block";
+  })
+
+  resumeButton.addEventListener("mouseleave", function (event) {
+    shades.style.display = "none";
+  })
+}
